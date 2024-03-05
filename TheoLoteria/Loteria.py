674dbcfdd8b87e44be031e5bbf2c3c35b1e5ed8b
Rafael Helfenstein Pereira <rafael_hp10@hotmail.com>
2024-03-04 21:36:18 -0400

    Update Loteria.py

--- TheoLoteria/Loteria.py
+++ TheoLoteria/Loteria.py
@@ -1,22 +1,33 @@
 #%%
 numero_sorte    = 7
 
-while   True:
-    numero = int(input("Entre com um numero entre 1 e 15!"))
 
-    if numero <= 0 and numero > 15:
-        continua = print("Valor invalido! Deseja continuar! [sim,não]")
-        if continua == 'sim':
-            pass
-        else:
-            break
-    else:
-        if numero != numero_sorte:
-            continua = input("Errou! Deseja continuar! [sim,não]")
+
+
+while True:
+    try:
+ 
+        numero = int(input("Entre com um numero entre 1 e 15!"))
+    
+        if numero <= 0 and numero > 15:
+            continua = print("Valor invalido! Deseja continuar! [sim,não]")
             if continua == 'sim':
                 pass
             else:
+                print("Mais sorte na próxima vez!")
                 break
         else:
-            print("Acerto miseravi!")
-            break
+            if numero != numero_sorte:
+                continua = input("Errou! Deseja continuar! [sim,não]")
+                if continua == 'sim':
+                    pass
+                else:
+                    print("Mais sorte na próxima vez!")
+                    break
+            else:
+                print("Acerto miseravi!")
+                break
+
+    except:
+        print("Valor invalido tente novamente!")
+        pass
